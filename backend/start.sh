2cb824b02d99ab4ebef5d542f992c60a80f0a788
moudalif <moudalifhamed@gmail.com>
2026-07-24 21:49:26 +0100

    Fix Gunicorn startup and enable stdout debug logs

--- backend/start.sh
+++ backend/start.sh
@@ -4,4 +4,8 @@
 
 # Start Gunicorn via python -m to avoid path issues when the gunicorn script is not available
 exec python -m gunicorn bouano.wsgi \
-	--error-logfile -
+  --workers 2 \
+  --bind 0.0.0.0:$PORT \
+  --log-level debug \
+  --access-logfile - \
+  --error-logfile -
