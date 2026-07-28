0fea6d2acacb4423912ebe19793855e334b8464a
moudalif <moudalifhamed@gmail.com>
2026-07-28 02:17:40 +0100

    Modification texte

--- frontend/src/data/campaignPartners.js
+++ frontend/src/data/campaignPartners.js
@@ -2,13 +2,13 @@ export const CAMPAIGN_PARTNERS = [
   { name: 'Nestlé', shortName: 'Nestlé', logo: '/images/partners/nestle.png' },
   {
     name: "Faculté de Médecine et des Sciences Pharmaceutiques de l'Université de Douala",
-    shortName: 'FMSP · Douala',
+    shortName: ' Port Autonome de Douala',
     logo: '/images/partners/fmsp-douala.png',
   },
   { name: 'TX Biomarker', shortName: 'TX Biomarker', logo: '/images/partners/tx-biomarker.png' },
   {
     name: "Association des Étudiants Ressortissants de l'Est (ASSERES)",
-    shortName: 'ASSERES',
+    shortName: 'Ministère de la santé publique',
     logo: '/images/partners/asseres-est.png',
   },
   { name: 'Croix-Rouge Camerounaise', shortName: 'Croix-Rouge Cameroun', logo: '/images/partners/croix-rouge-cameroun.png' },
